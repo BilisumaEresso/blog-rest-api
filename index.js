acae096be1d44672e82e22bebb17157f3890dd0d
BilisumaEreso <bilisuma.ugr-4236-16@aau.edu.et>
2025-10-16 10:47:58 +0300

    configured port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http=require("http")
 const app=require("./app")
 const {port}=require("./config/keys")
 
+const PORT=port||5000
 
 // create server
 const server=http.createServer(app)
@@ -9,5 +10,5 @@ const server=http.createServer(app)
 
 // listenning to server
 server.listen(port,()=>{
-    console.log(`running on server port ${port}`)
+    console.log(`running on server port ${PORT}`)
 })
